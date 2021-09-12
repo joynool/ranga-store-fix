e8b133d0595ba5e798b1c567125420d8c8462436
JoynoolAbedinMithoon <mithun55@gmail.com>
2021-09-13 05:38:30 +0600

    all funtionality added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+const allProducts = document.getElementById('all-products');
+const singleProduct = document.getElementById('single-display')
+
+// load all product function
 const loadProducts = () =>
 {
-  const url = `https://fakestoreapi.com/products`;
-  fetch(url)
-    .then((response) => response.json())
+  singleProduct.textContent = '';
+  fetch(`https://fakestoreapi.com/products`)
+    .then((res) => res.json())
     .then((data) => showProducts(data));
 };
 loadProducts();
@@ -10,44 +14,82 @@ loadProducts();
 // show all product in UI 
 const showProducts = (products) =>
 {
+  allProducts.textContent = '';
   products.forEach(items =>
   {
     const { category, description, id, image, price, rating: { rate, count }, title } = items;
-    console.log(image)
-    const div = document.createElement("div");
-    div.classList.add("col");
+    const div = document.createElement('div');
+    div.classList.add('col');
     div.innerHTML = `
-    <div id="single-product" class="card shadow p-2">
-      <img src="${image}" id="product-image" class="card-img-top img-thumbnail" alt="...">
-      <div class="card-body text-center">
-        <h5 class="card-title">${title}</h5>
-        <p class="card-text">Category: ${category}</p>
-        <p class="card-text">
-        <small class="text-muted">Ratings : ${rate} out of 5</small>
-        <br>
-        <small class="text-muted">Customer review : ${count}</small>
-        </p>
-        <h2>Price: $ ${price}</h2>
-      </div>
-      <div class="mx-auto">
-        <button onclick="addToCart(${id},${price})" id="addToCart-btn" class="buy-now btn btn-secondary">add to cart</button>
-        <button id="details-btn" class="btn btn-outline-secondary">Details</button>
-      </div>
+  <div id="single-product" class="card shadow p-2">
+    <img src="${image}" id="product-image" class="card-img-top img-thumbnail" alt="...">
+    <div class="card-body text-center">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text fw-bold">Category : ${category}</p>
+      <p class="card-text">
+      <small class="text-muted">Ratings : ${rate} out of 5</small>
+      <br>
+      <small class="text-muted">Customer review : ${count}</small>
+      </p>
+      <h2>Price: $ ${price}</h2>
     </div>
-    `;
-    document.getElementById("all-products").appendChild(div);
-  })
+    <div class="mx-auto">
+      <button onclick="addToCart(${price})" id="addToCart-btn" class="buy-now btn btn-secondary">add to cart</button>
+      <button onclick="itemDetails(${id})" id="details-btn" class="btn btn-outline-secondary">Details</button>
+    </div>
+  </div>
+  `;
+    allProducts.appendChild(div);
+  });
 };
+
+// update product count and my-cart price
 let count = 0;
-const addToCart = (id, price) =>
+const addToCart = (price) =>
 {
   count = count + 1;
-  updatePrice(id, price);
-
+  updatePrice("price", price);
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
+// single product details onclick handler
+const itemDetails = (id) =>
+{
+  allProducts.textContent = '';
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(res => res.json())
+    .then(data => SingleProductDisplay(data));
+}
+
+// show single product UI
+const SingleProductDisplay = (item) =>
+{
+  const { category, description, image, price, rating: { rate, count }, title } = item;
+  const div = document.createElement('div');
+  div.classList.add('col');
+  div.innerHTML = `
+  <div id="single-product" class="card shadow p-2">
+    <img src="${image}" class="card-img-top img-thumbnail" alt="...">
+    <div class="card-body text-center">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text fw-bold">Category : ${category}</p>
+      <p class="card-text fw-light">${description}</p>
+      <p class="card-text">
+      <small class="text-muted">Ratings : ${rate} out of 5</small>
+      <br>
+      <small class="text-muted">Customer review : ${count}</small>
+      </p>
+      <h2>Price: $ ${price}</h2>
+      <button onclick="addToCart(${price})" id="addToCart-btn" class="buy-now btn btn-secondary">add to cart</button>
+    </div>
+  </div>
+  `;
+  singleProduct.appendChild(div);
+}
+
+// get input value function
 const getInputValue = (id) =>
 {
   const element = document.getElementById(id).innerText;
@@ -61,15 +103,13 @@ const updatePrice = (id, value) =>
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) =>
 {
-  let sum = value.toFixed(2);
-  sum = parseFloat(sum);
-  document.getElementById(id).innerText = sum;
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -90,11 +130,11 @@ const updateTaxAndCharge = () =>
   }
 };
 
-//grandTotal update function
+// grandTotal update function
 const updateTotal = () =>
 {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
